bb7a4d56853064d7d62df3a05e53c286ba90789a
Domin <Domin@DESKTOP-TLOVUIR>
2023-01-03 20:47:15 +0100

    .

---
+++ mario/ScoreVypis.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mario
+{
+    public partial class ScoreVypis : Form
+    {
+        public ScoreVypis()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
